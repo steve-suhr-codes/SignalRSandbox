e949d78151657fda25b38eaae5970721ae772d80
Steve Suhr <stevesuhr@Steves-MacBook-Air.local>
2024-07-23 08:19:14 -0700

    finished with simple chat

---
+++ README.md
@@ -0,0 +1,2 @@
+A sandbox for playing around with SignalR.
+Originally started with https://learn.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-6.0&tabs=visual-studio-mac
--- SignalRSandbox/Program.cs
+++ SignalRSandbox/Program.cs
@@ -1,7 +1,10 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using SignalRSandbox.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -21,6 +24,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
 
